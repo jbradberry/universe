f7e7c11e44b24a16303380eaf35930fc71c6d5bc
Jeff Bradberry <jeff.bradberry@gmail.com>
2013-08-11 00:49:41 -0400

    Modified generate() to correctly deal with movement updates.

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -4,27 +4,62 @@ from universe import engine
 
 class MovementTestCase(unittest.TestCase):
     def test_empty_universe(self):
-        state = {'turn': 2500, 'width': 1000, 'locatables': ()}
-        updates = {}
+        state = {'turn': 2500, 'width': 1000, 'locatables': []}
+        updates = []
 
         results = engine.generate(state, updates)
         self.assertIsNotNone(results)
         new_state, new_updates = results
 
-        self.assertEqual(new_updates, {})
-        self.assertEqual(new_state, {'turn': 2501, 'width': 1000, 'locatables': ()})
+        self.assertEqual(new_updates, [])
+        self.assertEqual(new_state, {'turn': 2501, 'width': 1000, 'locatables': []})
 
     def test_one_stationary_object(self):
         state = {'turn': 2500, 'width': 1000,
-                 'locatables': ({'x': 456, 'y': 337, 'z': 441},)}
-        updates = {}
+                 'locatables': [{'id': 0, 'x': 456, 'y': 337, 'z': 441}]}
+        updates = []
 
         results = engine.generate(state, updates)
         self.assertIsNotNone(results)
         new_state, new_updates = results
 
-        self.assertEqual(new_updates, {})
+        self.assertEqual(new_updates, [])
         self.assertEqual(set(new_state.iterkeys()), set(['turn', 'width', 'locatables']))
         self.assertEqual(new_state['turn'], 2501)
         self.assertEqual(new_state['width'], 1000)
-        self.assertEqual(new_state['locatables'], ({'x': 456, 'y': 337, 'z': 441},))
+        self.assertEqual(new_state['locatables'], [{'id': 0, 'x': 456, 'y': 337, 'z': 441}])
+
+    def test_one_object_single_turn_move(self):
+        state = {'turn': 2500, 'width': 1000,
+                 'locatables': [{'id': 0, 'x': 480, 'y': 235, 'z': 187}]}
+        # actual speed is speed**2
+        updates = [{'locatable_id': 0, 'seq': 1, 'x_t': 422, 'y_t': 210, 'z_t': 178, 'speed': 10}]
+
+        results = engine.generate(state, updates)
+        self.assertIsNotNone(results)
+        new_state, new_updates = results
+
+        self.assertEqual(new_updates, [])
+        self.assertEqual(set(new_state.iterkeys()), set(['turn', 'width', 'locatables']))
+        self.assertEqual(new_state['turn'], 2501)
+        self.assertEqual(new_state['width'], 1000)
+        self.assertEqual(len(new_state['locatables']), 1)
+        self.assertEqual(new_state['locatables'], [{'id': 0, 'x': 422, 'y': 210, 'z': 178}])
+
+    def test_multi_turn_single_move(self):
+        state = {'turn': 2500, 'width': 1000,
+                 'locatables': [{'id': 0, 'x': 480, 'y': 235, 'z': 187}]}
+        # actual speed is speed**2
+        updates = [{'locatable_id': 0, 'seq': 1, 'x_t': 168, 'y_t': 870, 'z_t': 390, 'speed': 10}]
+
+        results = engine.generate(state, updates)
+        self.assertIsNotNone(results)
+        new_state, new_updates = results
+
+        self.assertEqual(len(new_updates), 1)
+        self.assertEqual(new_updates, updates)
+        self.assertEqual(set(new_state.iterkeys()), set(['turn', 'width', 'locatables']))
+        self.assertEqual(new_state['turn'], 2501)
+        self.assertEqual(new_state['width'], 1000)
+        self.assertEqual(len(new_state['locatables']), 1)
+        self.assertEqual(new_state['locatables'], [{'id': 0, 'x': 438, 'y': 321, 'z': 215}])
--- universe/engine.py
+++ universe/engine.py
@@ -1,10 +1,47 @@
+from __future__ import division
+from decimal import Decimal
+
 
 def generate(state, updates):
     new_state = {'turn': state['turn'] + 1, 'width': state['width']}
-    new_updates = {}
 
-    new_state['locatables'] = tuple(
-        locatable.copy() for locatable in state['locatables']
-    )
+    update_by_id_seq = dict(((update['locatable_id'], update['seq']), update)
+                            for update in updates)
+
+    update_by_id = {}
+    for (loc_id, loc_seq), update in sorted(update_by_id_seq.iteritems()):
+        update_by_id.setdefault(loc_id, []).append(update)
+
+    new_locatables = []
+    for locatable in state['locatables']:
+        new_loc = locatable.copy()
+        if new_loc['id'] in update_by_id:
+            update = update_by_id[new_loc['id']][0]
+            x, y, z = new_loc['x'], new_loc['y'], new_loc['z']
+            x_t, y_t, z_t = update['x_t'], update['y_t'], update['z_t']
+            # REVIEW: the warp-to-speed calculation should be a
+            # function brought in by the ruleset or game config
+            speed = update['speed'] ** 2
+
+            dx = x_t - x
+            dy = y_t - y
+            dz = z_t - z
+            D = Decimal(dx**2 + dy**2 + dz**2).sqrt()
+
+            if D.to_integral_value() <= speed:
+                x_new, y_new, z_new = x_t, y_t, z_t
+                update_by_id[new_loc['id']].pop(0)
+            else:
+                x_new = x + int((speed * dx / D).to_integral_value())
+                y_new = y + int((speed * dy / D).to_integral_value())
+                z_new = z + int((speed * dz / D).to_integral_value())
+
+            new_loc.update(x=x_new, y=y_new, z=z_new)
+        new_locatables.append(new_loc)
+
+    new_state['locatables'] = new_locatables
+
+    new_updates = [update for loc_id, loc_updates in update_by_id.iteritems()
+                   for update in loc_updates]
 
     return new_state, new_updates
