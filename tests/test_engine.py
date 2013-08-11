79d8edf3551a2b53778458fd0df15f9d6554aaa6
Jeff Bradberry <jeff.bradberry@gmail.com>
2013-08-10 22:09:27 -0400

    Update generation to allow for locatable objects to be carried over.

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -13,3 +13,18 @@ class MovementTestCase(unittest.TestCase):
 
         self.assertEqual(new_updates, {})
         self.assertEqual(new_state, {'turn': 2501, 'width': 1000, 'locatables': ()})
+
+    def test_one_stationary_object(self):
+        state = {'turn': 2500, 'width': 1000,
+                 'locatables': ({'x': 456, 'y': 337, 'z': 441},)}
+        updates = {}
+
+        results = engine.generate(state, updates)
+        self.assertIsNotNone(results)
+        new_state, new_updates = results
+
+        self.assertEqual(new_updates, {})
+        self.assertEqual(set(new_state.iterkeys()), set(['turn', 'width', 'locatables']))
+        self.assertEqual(new_state['turn'], 2501)
+        self.assertEqual(new_state['width'], 1000)
+        self.assertEqual(new_state['locatables'], ({'x': 456, 'y': 337, 'z': 441},))
--- universe/engine.py
+++ universe/engine.py
@@ -1,6 +1,10 @@
 
 def generate(state, updates):
-    new_state = {'turn': state['turn'] + 1, 'width': state['width'], 'locatables': ()}
+    new_state = {'turn': state['turn'] + 1, 'width': state['width']}
     new_updates = {}
+
+    new_state['locatables'] = tuple(
+        locatable.copy() for locatable in state['locatables']
+    )
 
     return new_state, new_updates
