3776250f91c00b0ac87ce2abec66506b71491c83
Jeff Bradberry <jeff.bradberry@gmail.com>
2013-09-10 21:55:15 -0400

    Converted the generate function into GameState class.

--- universe/engine.py
+++ universe/engine.py
@@ -2,46 +2,66 @@ from __future__ import division
 from decimal import Decimal
 
 
-def generate(state, updates):
-    new_state = {'turn': state['turn'] + 1, 'width': state['width']}
+class GameState(object):
+    def __init__(self, state, updates):
+        self.old = state
+        self.updates = updates
 
-    update_by_id_seq = dict(((update['locatable_id'], update['seq']), update)
-                            for update in updates)
+        self.new = {}
 
-    update_by_id = {}
-    for (loc_id, loc_seq), update in sorted(update_by_id_seq.iteritems()):
-        update_by_id.setdefault(loc_id, []).append(update)
+    def generate(self):
+        self.new_headers()
+        self.merge_updates()
+        self.process_movement()
 
-    new_locatables = []
-    for locatable in state['locatables']:
-        new_loc = locatable.copy()
-        if new_loc['id'] in update_by_id:
-            update = update_by_id[new_loc['id']][0]
-            x, y, z = new_loc['x'], new_loc['y'], new_loc['z']
-            x_t, y_t, z_t = update['x_t'], update['y_t'], update['z_t']
-            # REVIEW: the warp-to-speed calculation should be a
-            # function brought in by the ruleset or game config
-            speed = update['speed'] ** 2
+        return self.new
 
-            dx = x_t - x
-            dy = y_t - y
-            dz = z_t - z
-            D = Decimal(dx**2 + dy**2 + dz**2).sqrt()
+    def new_headers(self):
+        self.new.update(turn=self.old['turn'] + 1, width=self.old['width'])
 
-            if D.to_integral_value() <= speed:
-                x_new, y_new, z_new = x_t, y_t, z_t
-                update_by_id[new_loc['id']].pop(0)
-            else:
-                x_new = x + int((speed * dx / D).to_integral_value())
-                y_new = y + int((speed * dy / D).to_integral_value())
-                z_new = z + int((speed * dz / D).to_integral_value())
+    def merge_updates(self):
+        # deduplicate and index by locatable_id + sequence number
+        indexed_actions = dict(
+            ((loc_id, action['seq']), action)
+            for loc_id, action in self.old['actions'].iteritems()
+        )
 
-            new_loc.update(x=x_new, y=y_new, z=z_new)
-        new_locatables.append(new_loc)
+        indexed_actions.update(
+            ((action['locatable_id'], action['seq']), action)
+            for action in self.updates
+        )
 
-    new_state['locatables'] = new_locatables
+        self.actions = {}
+        for (loc_id, loc_seq), action in sorted(indexed_actions.iteritems()):
+            self.actions.setdefault(loc_id, []).append(action)
 
-    new_updates = [update for loc_id, loc_updates in update_by_id.iteritems()
-                   for update in loc_updates]
+    def process_movement(self):
+        locatables = []
+        for locatable in self.old['locatables']:
+            new_loc = locatable.copy()
+            if new_loc['id'] in self.actions:
+                action = self.actions[new_loc['id']][0]
 
-    return new_state, new_updates
+                x, y, z = new_loc['x'], new_loc['y'], new_loc['z']
+                x_t, y_t, z_t = action['x_t'], action['y_t'], action['z_t']
+                speed = action['speed'] ** 2
+
+                dx, dy, dz = x_t - x, y_t - y, z_t - z
+                D = Decimal(dx**2 + dy**2 + dz**2).sqrt()
+
+                if D.to_integral_value() <= speed:
+                    x_new, y_new, z_new = x_t, y_t, z_t
+                    self.actions[new_loc['id']].pop(0)
+                    if not self.actions[new_loc['id']]:
+                        del self.actions[new_loc['id']]
+                else:
+                    x_new = x + int((speed * dx / D).to_integral_value())
+                    y_new = y + int((speed * dy / D).to_integral_value())
+                    z_new = z + int((speed * dz / D).to_integral_value())
+
+                new_loc.update(x=x_new, y=y_new, z=z_new)
+
+            locatables.append(new_loc)
+
+        self.new['locatables'] = locatables
+        self.new['actions'] = self.actions
