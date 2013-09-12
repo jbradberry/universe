08a0f9042a199f3660f69ec4ecdd746eb78e5b66
Jeff Bradberry <jeff.bradberry@gmail.com>
2013-09-11 21:10:06 -0400

    Turned master state data into dicts and removed bookkeeping ids.

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -4,7 +4,7 @@ from universe import engine
 
 class MovementTestCase(unittest.TestCase):
     def test_empty_universe(self):
-        state = {'turn': 2500, 'width': 1000, 'locatables': [], 'actions': {}}
+        state = {'turn': 2500, 'width': 1000, 'locatables': {}, 'actions': {}}
         updates = []
 
         S = engine.GameState(state, updates)
@@ -12,11 +12,11 @@ class MovementTestCase(unittest.TestCase):
 
         self.assertIsNotNone(results)
         self.assertEqual(results,
-                         {'turn': 2501, 'width': 1000, 'locatables': [], 'actions': {}})
+                         {'turn': 2501, 'width': 1000, 'locatables': {}, 'actions': {}})
 
     def test_one_stationary_object(self):
         state = {'turn': 2500, 'width': 1000,
-                 'locatables': [{'id': 0, 'x': 456, 'y': 337, 'z': 441}],
+                 'locatables': {0: {'x': 456, 'y': 337, 'z': 441}},
                  'actions': {}}
         updates = []
 
@@ -27,15 +27,15 @@ class MovementTestCase(unittest.TestCase):
         self.assertEqual(set(results.iterkeys()), set(['turn', 'width', 'locatables', 'actions']))
         self.assertEqual(results['turn'], 2501)
         self.assertEqual(results['width'], 1000)
-        self.assertEqual(results['locatables'], [{'id': 0, 'x': 456, 'y': 337, 'z': 441}])
+        self.assertEqual(results['locatables'], {0: {'x': 456, 'y': 337, 'z': 441}})
         self.assertEqual(results['actions'], {})
 
     def test_one_object_single_turn_move(self):
         state = {'turn': 2500, 'width': 1000,
-                 'locatables': [{'id': 0, 'x': 480, 'y': 235, 'z': 187}],
+                 'locatables': {0: {'x': 480, 'y': 235, 'z': 187}},
                  'actions': {}}
         # actual speed is speed**2
-        updates = [{'locatable_id': 0, 'seq': 1, 'x_t': 422, 'y_t': 210, 'z_t': 178, 'speed': 10}]
+        updates = [{'locatable_id': 0, 'seq': 0, 'x_t': 422, 'y_t': 210, 'z_t': 178, 'speed': 10}]
 
         S = engine.GameState(state, updates)
         results = S.generate()
@@ -45,15 +45,15 @@ class MovementTestCase(unittest.TestCase):
         self.assertEqual(results['turn'], 2501)
         self.assertEqual(results['width'], 1000)
         self.assertEqual(len(results['locatables']), 1)
-        self.assertEqual(results['locatables'], [{'id': 0, 'x': 422, 'y': 210, 'z': 178}])
+        self.assertEqual(results['locatables'], {0: {'x': 422, 'y': 210, 'z': 178}})
         self.assertEqual(results['actions'], {})
 
     def test_multi_turn_single_move(self):
         state = {'turn': 2500, 'width': 1000,
-                 'locatables': [{'id': 0, 'x': 480, 'y': 235, 'z': 187}],
+                 'locatables': {0: {'x': 480, 'y': 235, 'z': 187}},
                  'actions': {}}
         # actual speed is speed**2
-        updates = [{'locatable_id': 0, 'seq': 1, 'x_t': 168, 'y_t': 870, 'z_t': 390, 'speed': 10}]
+        updates = [{'locatable_id': 0, 'seq': 0, 'x_t': 168, 'y_t': 870, 'z_t': 390, 'speed': 10}]
 
         S = engine.GameState(state, updates)
         results = S.generate()
@@ -63,6 +63,6 @@ class MovementTestCase(unittest.TestCase):
         self.assertEqual(results['turn'], 2501)
         self.assertEqual(results['width'], 1000)
         self.assertEqual(len(results['locatables']), 1)
-        self.assertEqual(results['locatables'], [{'id': 0, 'x': 438, 'y': 321, 'z': 215}])
+        self.assertEqual(results['locatables'], {0: {'x': 438, 'y': 321, 'z': 215}})
         self.assertEqual(results['actions'],
-                         {0: [{'locatable_id': 0, 'seq': 1, 'x_t': 168, 'y_t': 870, 'z_t': 390, 'speed': 10}]})
+                         {0: [{'x_t': 168, 'y_t': 870, 'z_t': 390, 'speed': 10}]})
--- universe/engine.py
+++ universe/engine.py
@@ -13,6 +13,7 @@ class GameState(object):
         self.new_headers()
         self.merge_updates()
         self.process_movement()
+        self.post_process()
 
         return self.new
 
@@ -20,48 +21,51 @@ class GameState(object):
         self.new.update(turn=self.old['turn'] + 1, width=self.old['width'])
 
     def merge_updates(self):
-        # deduplicate and index by locatable_id + sequence number
         indexed_actions = dict(
-            ((loc_id, action['seq']), action)
-            for loc_id, action in self.old['actions'].iteritems()
+            (loc_id, dict(enumerate(action_list)))
+            for loc_id, action_list in self.old['actions'].iteritems()
         )
 
-        indexed_actions.update(
-            ((action['locatable_id'], action['seq']), action)
-            for action in self.updates
-        )
+        for action in self.updates:
+            loc_id = action.pop('locatable_id')
+            seq = action.pop('seq')
+            indexed_actions.setdefault(loc_id, {})[seq] = action
 
-        self.actions = {}
-        for (loc_id, loc_seq), action in sorted(indexed_actions.iteritems()):
-            self.actions.setdefault(loc_id, []).append(action)
+        self.actions = dict(
+            (loc_id, [action for seq, action in sorted(actions.iteritems())])
+            for loc_id, actions in indexed_actions.iteritems()
+        )
 
     def process_movement(self):
-        locatables = []
-        for locatable in self.old['locatables']:
-            new_loc = locatable.copy()
-            if new_loc['id'] in self.actions:
-                action = self.actions[new_loc['id']][0]
+        for loc_id, actions in self.actions.iteritems():
+            action = actions[0]
+            locatable = self.old['locatables'][loc_id]
 
-                x, y, z = new_loc['x'], new_loc['y'], new_loc['z']
-                x_t, y_t, z_t = action['x_t'], action['y_t'], action['z_t']
-                speed = action['speed'] ** 2
+            x, y, z = locatable['x'], locatable['y'], locatable['z']
+            x_t, y_t, z_t = action['x_t'], action['y_t'], action['z_t']
+            speed = action['speed'] ** 2
 
-                dx, dy, dz = x_t - x, y_t - y, z_t - z
-                D = Decimal(dx**2 + dy**2 + dz**2).sqrt()
+            dx, dy, dz = x_t - x, y_t - y, z_t - z
+            D = Decimal(dx**2 + dy**2 + dz**2).sqrt()
 
-                if D.to_integral_value() <= speed:
-                    x_new, y_new, z_new = x_t, y_t, z_t
-                    self.actions[new_loc['id']].pop(0)
-                    if not self.actions[new_loc['id']]:
-                        del self.actions[new_loc['id']]
-                else:
-                    x_new = x + int((speed * dx / D).to_integral_value())
-                    y_new = y + int((speed * dy / D).to_integral_value())
-                    z_new = z + int((speed * dz / D).to_integral_value())
+            if D.to_integral_value() <= speed:
+                x_new, y_new, z_new = x_t, y_t, z_t
+                actions.pop(0)
+            else:
+                x_new = x + int((speed * dx / D).to_integral_value())
+                y_new = y + int((speed * dy / D).to_integral_value())
+                z_new = z + int((speed * dz / D).to_integral_value())
 
-                new_loc.update(x=x_new, y=y_new, z=z_new)
+            locatable.update(x=x_new, y=y_new, z=z_new)
 
-            locatables.append(new_loc)
-
-        self.new['locatables'] = locatables
+        self.new['locatables'] = self.old['locatables']
         self.new['actions'] = self.actions
+
+    def post_process(self):
+        loc_ids = [
+            loc_id for loc_id, actions in self.new['actions'].iteritems()
+            if not actions
+        ]
+
+        for loc_id in loc_ids:
+            del self.new['actions'][loc_id]
