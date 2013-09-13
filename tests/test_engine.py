c766e97f9ba62fee3c874a98b9030c95a1ac0b81
Jeff Bradberry <jeff.bradberry@gmail.com>
2013-09-12 21:54:38 -0400

    Added ability for moves to target other locatables.
    Also, since this can result in dependency cycles, added the ability to randomly choose a unit to move early to break the cycle.

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -10,7 +10,6 @@ class MovementTestCase(unittest.TestCase):
         S = engine.GameState(state, updates)
         results = S.generate()
 
-        self.assertIsNotNone(results)
         self.assertEqual(results,
                          {'turn': 2501, 'width': 1000, 'locatables': {}, 'actions': {}})
 
@@ -23,8 +22,6 @@ class MovementTestCase(unittest.TestCase):
         S = engine.GameState(state, updates)
         results = S.generate()
 
-        self.assertIsNotNone(results)
-        self.assertEqual(set(results.iterkeys()), set(['turn', 'width', 'locatables', 'actions']))
         self.assertEqual(results['turn'], 2501)
         self.assertEqual(results['width'], 1000)
         self.assertEqual(results['locatables'], {0: {'x': 456, 'y': 337, 'z': 441}})
@@ -40,11 +37,8 @@ class MovementTestCase(unittest.TestCase):
         S = engine.GameState(state, updates)
         results = S.generate()
 
-        self.assertIsNotNone(results)
-        self.assertEqual(set(results.iterkeys()), set(['turn', 'width', 'locatables', 'actions']))
         self.assertEqual(results['turn'], 2501)
         self.assertEqual(results['width'], 1000)
-        self.assertEqual(len(results['locatables']), 1)
         self.assertEqual(results['locatables'], {0: {'x': 422, 'y': 210, 'z': 178}})
         self.assertEqual(results['actions'], {})
 
@@ -58,11 +52,125 @@ class MovementTestCase(unittest.TestCase):
         S = engine.GameState(state, updates)
         results = S.generate()
 
-        self.assertIsNotNone(results)
-        self.assertEqual(set(results.iterkeys()), set(['turn', 'width', 'locatables', 'actions']))
         self.assertEqual(results['turn'], 2501)
         self.assertEqual(results['width'], 1000)
-        self.assertEqual(len(results['locatables']), 1)
         self.assertEqual(results['locatables'], {0: {'x': 438, 'y': 321, 'z': 215}})
         self.assertEqual(results['actions'],
                          {0: [{'x_t': 168, 'y_t': 870, 'z_t': 390, 'speed': 10}]})
+
+    def test_previously_queued_move(self):
+        state = {'turn': 2500, 'width': 1000,
+                 'locatables': {0: {'x': 480, 'y': 235, 'z': 187}},
+                 'actions': {0: [{'x_t': 422, 'y_t': 210, 'z_t': 178, 'speed': 10}]}}
+        updates = []
+
+        S = engine.GameState(state, updates)
+        results = S.generate()
+
+        self.assertEqual(results['turn'], 2501)
+        self.assertEqual(results['width'], 1000)
+        self.assertEqual(results['locatables'], {0: {'x': 422, 'y': 210, 'z': 178}})
+        self.assertEqual(results['actions'], {})
+
+    def test_replace_queued_move(self):
+        state = {'turn': 2500, 'width': 1000,
+                 'locatables': {0: {'x': 480, 'y': 235, 'z': 187}},
+                 'actions': {0: [{'x_t': 637, 'y_t': 786, 'z_t': 255, 'speed': 8}]}}
+        updates = [{'locatable_id': 0, 'seq': 0, 'x_t': 422, 'y_t': 210, 'z_t': 178, 'speed': 10}]
+
+        S = engine.GameState(state, updates)
+        results = S.generate()
+
+        self.assertEqual(results['turn'], 2501)
+        self.assertEqual(results['width'], 1000)
+        self.assertEqual(results['locatables'], {0: {'x': 422, 'y': 210, 'z': 178}})
+        self.assertEqual(results['actions'], {})
+
+    def test_add_to_queued_moves(self):
+        state = {'turn': 2500, 'width': 1000,
+                 'locatables': {0: {'x': 480, 'y': 235, 'z': 187}},
+                 'actions': {0: [{'x_t': 422, 'y_t': 210, 'z_t': 178, 'speed': 10}]}}
+        updates = [{'locatable_id': 0, 'seq': 1, 'x_t': 637, 'y_t': 786, 'z_t': 255, 'speed': 8}]
+
+        S = engine.GameState(state, updates)
+        results = S.generate()
+
+        self.assertEqual(results['turn'], 2501)
+        self.assertEqual(results['width'], 1000)
+        self.assertEqual(results['locatables'], {0: {'x': 422, 'y': 210, 'z': 178}})
+        self.assertEqual(results['actions'], {0: [{'x_t': 637, 'y_t': 786, 'z_t': 255, 'speed': 8}]})
+
+    def test_target_stationary_object(self):
+        state = {'turn': 2500, 'width': 1000,
+                 'locatables': {0: {'x': 480, 'y': 235, 'z': 187},
+                                1: {'x': 460, 'y': 215, 'z': 207}},
+                 'actions': {0: [{'target_id': 1, 'speed': 10}]}}
+        updates = []
+
+        S = engine.GameState(state, updates)
+        results = S.generate()
+
+        self.assertEqual(results['turn'], 2501)
+        self.assertEqual(results['width'], 1000)
+        self.assertEqual(results['locatables'], {0: {'x': 460, 'y': 215, 'z': 207},
+                                                 1: {'x': 460, 'y': 215, 'z': 207}})
+        self.assertEqual(results['actions'], {})
+
+    def test_target_moving_object(self):
+        state = {'turn': 2500, 'width': 1000,
+                 'locatables': {0: {'x': 480, 'y': 235, 'z': 187},
+                                1: {'x': 460, 'y': 215, 'z': 207}},
+                 'actions': {0: [{'target_id': 1, 'speed': 10}],
+                             1: [{'x_t': 465, 'y_t': 220, 'z_t': 197, 'speed': 10}]}}
+        updates = []
+
+        S = engine.GameState(state, updates)
+        results = S.generate()
+
+        self.assertEqual(results['turn'], 2501)
+        self.assertEqual(results['width'], 1000)
+        self.assertEqual(results['locatables'], {0: {'x': 465, 'y': 220, 'z': 197},
+                                                 1: {'x': 465, 'y': 220, 'z': 197}})
+        self.assertEqual(results['actions'], {})
+
+    def test_mutual_intercept(self):
+        state = {'turn': 2500, 'width': 1000,
+                 'locatables': {0: {'x': 480, 'y': 235, 'z': 187},
+                                1: {'x': 460, 'y': 215, 'z': 207}},
+                 'actions': {0: [{'target_id': 1, 'speed': 10}],
+                             1: [{'target_id': 0, 'speed': 10}]}}
+        updates = []
+
+        S = engine.GameState(state, updates)
+        results = S.generate()
+
+        self.assertEqual(results['turn'], 2501)
+        self.assertEqual(results['width'], 1000)
+        self.assertEqual(len(results['locatables']), 2)
+        self.assertEqual(results['locatables'][0], results['locatables'][1])
+        self.assertTrue(results['locatables'][0] == {'x': 480, 'y': 235, 'z': 187} or
+                        results['locatables'][0] == {'x': 460, 'y': 215, 'z': 207})
+        self.assertEqual(results['actions'], {})
+
+    def test_three_way_cycle_intercept(self):
+        state = {'turn': 2500, 'width': 1000,
+                 'locatables': {0: {'x': 480, 'y': 235, 'z': 187},
+                                1: {'x': 460, 'y': 215, 'z': 207},
+                                2: {'x': 500, 'y': 205, 'z': 190}},
+                 'actions': {0: [{'target_id': 1, 'speed': 10}],
+                             1: [{'target_id': 2, 'speed': 10}],
+                             2: [{'target_id': 0, 'speed': 10}]}}
+        updates = []
+
+        S = engine.GameState(state, updates)
+        results = S.generate()
+
+        self.assertEqual(results['turn'], 2501)
+        self.assertEqual(results['width'], 1000)
+        self.assertEqual(len(results['locatables']), 3)
+        self.assertEqual(results['locatables'][0], results['locatables'][1])
+        self.assertEqual(results['locatables'][0], results['locatables'][2])
+        self.assertTrue(results['locatables'][0] == {'x': 480, 'y': 235, 'z': 187} or
+                        results['locatables'][0] == {'x': 460, 'y': 215, 'z': 207} or
+                        results['locatables'][0] == {'x': 500, 'y': 205, 'z': 190})
+        self.assertEqual(results['actions'], {})
--- universe/engine.py
+++ universe/engine.py
@@ -1,5 +1,6 @@
 from __future__ import division
 from decimal import Decimal
+import random
 
 
 class GameState(object):
@@ -37,29 +38,55 @@ class GameState(object):
         )
 
     def process_movement(self):
-        for loc_id, actions in self.actions.iteritems():
-            action = actions[0]
-            locatable = self.old['locatables'][loc_id]
+        movements = dict((loc_id, actions.pop(0))
+                         for loc_id, actions in self.actions.iteritems()
+                         if actions)
+        while movements:
+            update = False
+            for loc_id in list(movements.iterkeys()):
+                move = movements[loc_id]
+                if 'target_id' not in move:
+                    self._do_move(loc_id, move)
+                elif move['target_id'] not in movements:
+                    self._do_move(loc_id, move)
+                else:
+                    continue
 
-            x, y, z = locatable['x'], locatable['y'], locatable['z']
-            x_t, y_t, z_t = action['x_t'], action['y_t'], action['z_t']
-            speed = action['speed'] ** 2
+                update = True
+                del movements[loc_id]
 
-            dx, dy, dz = x_t - x, y_t - y, z_t - z
-            D = Decimal(dx**2 + dy**2 + dz**2).sqrt()
-
-            if D.to_integral_value() <= speed:
-                x_new, y_new, z_new = x_t, y_t, z_t
-                actions.pop(0)
-            else:
-                x_new = x + int((speed * dx / D).to_integral_value())
-                y_new = y + int((speed * dy / D).to_integral_value())
-                z_new = z + int((speed * dz / D).to_integral_value())
-
-            locatable.update(x=x_new, y=y_new, z=z_new)
+            if not update:
+                loc_id = random.choice(movements.keys())
+                self._do_move(loc_id, movements[loc_id])
+                del movements[loc_id]
 
         self.new['locatables'] = self.old['locatables']
         self.new['actions'] = self.actions
+
+    def _do_move(self, loc_id, move):
+        locatable = self.old['locatables'][loc_id]
+
+        speed = move['speed'] ** 2
+        x, y, z = locatable['x'], locatable['y'], locatable['z']
+        if 'target_id' in move:
+            target = self.old['locatables'][move['target_id']]
+            x_t, y_t, z_t = target['x'], target['y'], target['z']
+        else:
+            x_t, y_t, z_t = move['x_t'], move['y_t'], move['z_t']
+
+        dx, dy, dz = x_t - x, y_t - y, z_t - z
+        D = Decimal(dx**2 + dy**2 + dz**2).sqrt()
+
+        if D.to_integral_value() <= speed:
+            x_new, y_new, z_new = x_t, y_t, z_t
+        else:
+            x_new = x + int((speed * dx / D).to_integral_value())
+            y_new = y + int((speed * dy / D).to_integral_value())
+            z_new = z + int((speed * dz / D).to_integral_value())
+            # waypoint isn't reached yet, so prepend back into the queue.
+            self.actions[loc_id].insert(0, move)
+
+        locatable.update(x=x_new, y=y_new, z=z_new)
 
     def post_process(self):
         loc_ids = [
