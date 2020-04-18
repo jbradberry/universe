6cbd122601d4343b5f8d18f4b86021633bd29ace
Jeff Bradberry <jeff.bradberry@gmail.com>
2020-04-18 15:12:17 -0400

    Add some new movement tests
    - slow movement doesn't get truncated
    - linear interception just at the edge of range still works
    - intercepting ships can be faked out and have to change course

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -111,6 +111,22 @@ class MovementTestCase(unittest.TestCase):
                  'queue': [{'x_t': 637, 'y_t': 786, 'warp': 8}]}}
         )
 
+    def test_slow_motion(self):
+        state = {
+            'turn': 2500, 'width': 1000,
+            'entities': {0: {'type': 'ship', 'x': 500, 'y': 500, 'queue': [{'x_t': 501, 'y_t': 500, 'warp': 1}]}}
+        }
+
+        S = engine.GameState(state, {})
+        results = S.generate()
+
+        self.assertEqual(results['turn'], 2501)
+        self.assertEqual(results['width'], 1000)
+        self.assertEqual(
+            results['entities'],
+            {0: {'type': 'ship', 'x': 501, 'x_prev': 500, 'y': 500, 'y_prev': 500, 'queue': []}}
+        )
+
     def test_target_stationary_object(self):
         state = {'turn': 2500, 'width': 1000,
                  'entities': {0: {'type': 'ship', 'x': 480, 'y': 235, 'queue': [{'target_id': 1, 'warp': 10}]},
@@ -129,9 +145,11 @@ class MovementTestCase(unittest.TestCase):
         )
 
     def test_target_moving_object(self):
-        state = {'turn': 2500, 'width': 1000,
-                 'entities': {0: {'type': 'ship', 'x': 480, 'y': 235, 'queue': [{'target_id': 1, 'warp': 10}]},
-                              1: {'type': 'ship', 'x': 460, 'y': 215, 'queue': [{'x_t': 465, 'y_t': 220, 'warp': 10}]}}}
+        state = {
+            'turn': 2500, 'width': 1000,
+            'entities': {0: {'type': 'ship', 'x': 480, 'y': 235, 'queue': [{'target_id': 1, 'warp': 10}]},
+                         1: {'type': 'ship', 'x': 460, 'y': 215, 'queue': [{'x_t': 465, 'y_t': 220, 'warp': 10}]}}
+        }
         updates = {}
 
         S = engine.GameState(state, updates)
@@ -143,6 +161,44 @@ class MovementTestCase(unittest.TestCase):
             results['entities'],
             {0: {'type': 'ship', 'x': 465, 'x_prev': 480, 'y': 220, 'y_prev': 235, 'queue': []},
              1: {'type': 'ship', 'x': 465, 'x_prev': 460, 'y': 220, 'y_prev': 215, 'queue': []}}
+        )
+
+    def test_target_can_barely_reach(self):
+        state = {
+            'turn': 2500, 'width': 1000,
+            'entities': {0: {'type': 'ship', 'x': 560, 'y': 315, 'queue': [{'target_id': 1, 'warp': 10}]},
+                         1: {'type': 'ship', 'x': 460, 'y': 215, 'queue': [{'x_t': 660, 'y_t': 215, 'warp': 10}]}}
+        }
+        S = engine.GameState(state, {})
+        results = S.generate()
+
+        self.assertEqual(results['turn'], 2501)
+        self.assertEqual(results['width'], 1000)
+        self.assertEqual(
+            results['entities'],
+            {0: {'type': 'ship', 'x': 560, 'x_prev': 560, 'y': 215, 'y_prev': 315, 'queue': []},
+             1: {'type': 'ship', 'x': 560, 'x_prev': 460, 'y': 215, 'y_prev': 215,
+                 'queue': [{'x_t': 660, 'y_t': 215, 'warp': 10}]}}
+        )
+
+    def test_target_stops_short(self):
+        state = {
+            'turn': 2500, 'width': 1000,
+            'entities': {0: {'type': 'ship', 'x': 560, 'y': 315, 'queue': [{'target_id': 1, 'warp': 10}]},
+                         1: {'type': 'ship', 'x': 460, 'y': 215, 'queue': [{'x_t': 510, 'y_t': 215, 'warp': 10}]}}
+        }
+        S = engine.GameState(state, {})
+        results = S.generate()
+
+        self.assertEqual(results['turn'], 2501)
+        self.assertEqual(results['width'], 1000)
+        # Ship 0 should have moved directly north for the first 50 ticks, then northwest for the remainder
+        # i.e. roughly 35 lightyears in each direction
+        self.assertEqual(
+            results['entities'],
+            {0: {'type': 'ship', 'x': 525, 'x_prev': 560, 'y': 230, 'y_prev': 315,
+                 'queue': [{'target_id': 1, 'warp': 10}]},
+             1: {'type': 'ship', 'x': 510, 'x_prev': 460, 'y': 215, 'y_prev': 215, 'queue': []}}
         )
 
     def test_mutual_intercept(self):
