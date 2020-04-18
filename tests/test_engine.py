29a1ab22216e0fff9c6a9ef17e2764593d3841e0
Jeff Bradberry <jeff.bradberry@gmail.com>
2020-04-18 14:39:22 -0400

    Split movement into N steps
    and attempt to project out intercept points based on what we know at a
    given step of the target's vector.

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -24,7 +24,10 @@ class MovementTestCase(unittest.TestCase):
 
         self.assertEqual(results['turn'], 2501)
         self.assertEqual(results['width'], 1000)
-        self.assertEqual(results['entities'], {0: {'type': 'ship', 'x': 456, 'y': 337, 'queue': []}})
+        self.assertEqual(
+            results['entities'],
+            {0: {'type': 'ship', 'x': 456, 'x_prev': 456, 'y': 337, 'y_prev': 337, 'queue': []}}
+        )
 
     def test_one_object_single_turn_move(self):
         state = {'turn': 2500, 'width': 1000,
@@ -37,7 +40,10 @@ class MovementTestCase(unittest.TestCase):
 
         self.assertEqual(results['turn'], 2501)
         self.assertEqual(results['width'], 1000)
-        self.assertEqual(results['entities'], {0: {'type': 'ship', 'x': 422, 'y': 210, 'queue': []}})
+        self.assertEqual(
+            results['entities'],
+            {0: {'type': 'ship', 'x': 422, 'x_prev': 480, 'y': 210, 'y_prev': 235, 'queue': []}}
+        )
 
     def test_multi_turn_single_move(self):
         state = {'turn': 2500, 'width': 1000,
@@ -50,12 +56,16 @@ class MovementTestCase(unittest.TestCase):
 
         self.assertEqual(results['turn'], 2501)
         self.assertEqual(results['width'], 1000)
-        self.assertEqual(results['entities'],
-            {0: {'type': 'ship', 'x': 436, 'y': 325, 'queue': [{'x_t': 168, 'y_t': 870, 'warp': 10}]}})
+        self.assertEqual(
+            results['entities'],
+            {0: {'type': 'ship', 'x': 436, 'x_prev': 480, 'y': 325, 'y_prev': 235,
+                 'queue': [{'x_t': 168, 'y_t': 870, 'warp': 10}]}}
+        )
 
     def test_previously_queued_move(self):
         state = {'turn': 2500, 'width': 1000,
-                 'entities': {0: {'type': 'ship', 'x': 480, 'y': 235, 'queue': [{'x_t': 422, 'y_t': 210, 'warp': 10}]}}}
+                 'entities': {
+                     0: {'type': 'ship', 'x': 480, 'y': 235, 'queue': [{'x_t': 422, 'y_t': 210, 'warp': 10}]}}}
         updates = {}
 
         S = engine.GameState(state, updates)
@@ -63,11 +73,15 @@ class MovementTestCase(unittest.TestCase):
 
         self.assertEqual(results['turn'], 2501)
         self.assertEqual(results['width'], 1000)
-        self.assertEqual(results['entities'], {0: {'type': 'ship', 'x': 422, 'y': 210, 'queue': []}})
+        self.assertEqual(
+            results['entities'],
+            {0: {'type': 'ship', 'x': 422, 'x_prev': 480, 'y': 210, 'y_prev': 235, 'queue': []}}
+        )
 
     def test_replace_queued_move(self):
         state = {'turn': 2500, 'width': 1000,
-                 'entities': {0: {'type': 'ship', 'x': 480, 'y': 235, 'queue': [{'x_t': 637, 'y_t': 786, 'warp': 8}]}}}
+                 'entities': {
+                     0: {'type': 'ship', 'x': 480, 'y': 235, 'queue': [{'x_t': 637, 'y_t': 786, 'warp': 8}]}}}
         updates = {0: [{'seq': 0, 'x_t': 422, 'y_t': 210, 'warp': 10}]}
 
         S = engine.GameState(state, updates)
@@ -75,11 +89,15 @@ class MovementTestCase(unittest.TestCase):
 
         self.assertEqual(results['turn'], 2501)
         self.assertEqual(results['width'], 1000)
-        self.assertEqual(results['entities'], {0: {'type': 'ship', 'x': 422, 'y': 210, 'queue': []}})
+        self.assertEqual(
+            results['entities'],
+            {0: {'type': 'ship', 'x': 422, 'x_prev': 480, 'y': 210, 'y_prev': 235, 'queue': []}}
+        )
 
     def test_add_to_queued_moves(self):
         state = {'turn': 2500, 'width': 1000,
-                 'entities': {0: {'type': 'ship', 'x': 480, 'y': 235, 'queue': [{'x_t': 422, 'y_t': 210, 'warp': 10}]}}}
+                 'entities': {
+                     0: {'type': 'ship', 'x': 480, 'y': 235, 'queue': [{'x_t': 422, 'y_t': 210, 'warp': 10}]}}}
         updates = {0: [{'seq': 1, 'x_t': 637, 'y_t': 786, 'warp': 8}]}
 
         S = engine.GameState(state, updates)
@@ -87,7 +105,11 @@ class MovementTestCase(unittest.TestCase):
 
         self.assertEqual(results['turn'], 2501)
         self.assertEqual(results['width'], 1000)
-        self.assertEqual(results['entities'], {0: {'type': 'ship', 'x': 422, 'y': 210, 'queue': [{'x_t': 637, 'y_t': 786, 'warp': 8}]}})
+        self.assertEqual(
+            results['entities'],
+            {0: {'type': 'ship', 'x': 422, 'x_prev': 480, 'y': 210, 'y_prev': 235,
+                 'queue': [{'x_t': 637, 'y_t': 786, 'warp': 8}]}}
+        )
 
     def test_target_stationary_object(self):
         state = {'turn': 2500, 'width': 1000,
@@ -100,13 +122,16 @@ class MovementTestCase(unittest.TestCase):
 
         self.assertEqual(results['turn'], 2501)
         self.assertEqual(results['width'], 1000)
-        self.assertEqual(results['entities'], {0: {'type': 'ship', 'x': 460, 'y': 215, 'queue': []},
-                                               1: {'type': 'ship', 'x': 460, 'y': 215, 'queue': []}})
+        self.assertEqual(
+            results['entities'],
+            {0: {'type': 'ship', 'x': 460, 'x_prev': 480, 'y': 215, 'y_prev': 235, 'queue': []},
+             1: {'type': 'ship', 'x': 460, 'x_prev': 460, 'y': 215, 'y_prev': 215, 'queue': []}}
+        )
 
     def test_target_moving_object(self):
         state = {'turn': 2500, 'width': 1000,
                  'entities': {0: {'type': 'ship', 'x': 480, 'y': 235, 'queue': [{'target_id': 1, 'warp': 10}]},
-                                1: {'type': 'ship', 'x': 460, 'y': 215, 'queue': [{'x_t': 465, 'y_t': 220, 'warp': 10}]}}}
+                              1: {'type': 'ship', 'x': 460, 'y': 215, 'queue': [{'x_t': 465, 'y_t': 220, 'warp': 10}]}}}
         updates = {}
 
         S = engine.GameState(state, updates)
@@ -114,8 +139,11 @@ class MovementTestCase(unittest.TestCase):
 
         self.assertEqual(results['turn'], 2501)
         self.assertEqual(results['width'], 1000)
-        self.assertEqual(results['entities'], {0: {'type': 'ship', 'x': 465, 'y': 220, 'queue': []},
-                                                 1: {'type': 'ship', 'x': 465, 'y': 220, 'queue': []}})
+        self.assertEqual(
+            results['entities'],
+            {0: {'type': 'ship', 'x': 465, 'x_prev': 480, 'y': 220, 'y_prev': 235, 'queue': []},
+             1: {'type': 'ship', 'x': 465, 'x_prev': 460, 'y': 220, 'y_prev': 215, 'queue': []}}
+        )
 
     def test_mutual_intercept(self):
         state = {'turn': 2500, 'width': 1000,
@@ -156,9 +184,6 @@ class MovementTestCase(unittest.TestCase):
         self.assertEqual(results['entities'][2]['queue'], [])
 
     def test_four_way_cycle_intercept(self):
-        # ensures that the early-move deadlock breaker doesn't drop
-        # its waypoint when it fails to end the turn at the same
-        # location as its target.
         state = {'turn': 2500, 'width': 1000,
                  'entities': {0: {'type': 'ship', 'x': 500, 'y': 500, 'queue': [{'target_id': 1, 'warp': 10}]},
                               1: {'type': 'ship', 'x': 600, 'y': 500, 'queue': [{'target_id': 2, 'warp': 10}]},
@@ -172,10 +197,6 @@ class MovementTestCase(unittest.TestCase):
         self.assertEqual(results['turn'], 2501)
         self.assertEqual(results['width'], 1000)
         self.assertEqual(len(results['entities']), 4)
-        coordinates = [(entity['x'], entity['y'])
-                       for entity in results['entities'].values()]
-        self.assertEqual(len(set(coordinates)), 2)
-        # FIX ME!
-        # Once we change the implementation of how movement happens, we'll need to put in new
-        # assertions for that.
-        # self.assertTrue(all(entity['queue'] for entity in results['entities'].values()))
+        coordinates = {(entity['x'], entity['y']) for entity in results['entities'].values()}
+        self.assertEqual(len(coordinates), 1)
+        self.assertTrue(all(not entity['queue'] for entity in results['entities'].values()))
